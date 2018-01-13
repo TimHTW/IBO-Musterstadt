e811936f9035ad848894e8bb7546d80f6583a60e
TimHTW <s76782@htw-dresden.de>
2018-01-13 15:55:38 +0100

    basic structure

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var process_id;
 var url = "process.json";
-
+get_stakeholder();
 $.ajax({
     url: url
 }).then(function(data){
     $.each(data.process.children, function(key,val){
-        $('.grid-container .header #select_process').append($('<option>', {
+        $('#header #select_process').append($('<option>', {
             id: val.id,
             value: val.name,
             text: val.name
@@ -26,10 +26,29 @@ function get_child_process(id) {
             if (val.id == id){
                 var name = val.name;
                 var description = val.description;
-                $('.content').empty();
-                $('.content').append("<h4>"+name+"</h4>");
+                $('#content .tab-content #procces').empty();
+                $('#content .tab-content #procces').append("<h4>"+name+"</h4>");
+                $('#mytabs a[href="#procces"]').tab('show');
                 return false;
             }
+        });
+    });
+}
+
+function get_stakeholder() {
+    var name,
+        email,
+        website;
+    $.ajax({
+        url: url
+    }).then(function(data){
+        $.each(data.process.stakeholder, function(key, val){
+            name = val.name;
+            $.each(val.contact, function(key,val){
+                email = "example@mail.de";
+                website = "example-website.com";
+            });
+            $('#content .tab-content #stakeholder table tbody').append("<tr><td>"+name+"</td><td>"+email+"</td><td>"+website+"</td></tr>");
         });
     });
 }
